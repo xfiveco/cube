1c4cafb08717e5cf7298b74992633beb53be6f98
Artur Kot <arturkot85@gmail.com>
2014-02-17 12:20:44 +0100

    Updates: * add script auto-generating navigation in the index page * add Cube Texture, Compatibility, and Documentation sections to the index page

--- gh-pages-js/generate-nav.js
+++ gh-pages-js/generate-nav.js
@@ -1,10 +1,57 @@
-(function () {
-	var sections = document.getElementsByName('section');
-		// $h2s = document.getElementsByName('h2'),
-		// $h3s = document.getElementsByName('h3'),
-		// $h4s = document.getElementsByName('h4');
+/*jshint browser: true, smarttabs: false, indent: 4, undef: true, unused: true, strict: true, trailing: true, onevar: true, white: true, laxbreak: true */
 
-	for (section in sections) {
-		console.log(section);
-	}
+(function () {
+    'use strict';
+
+    var $insertNav = document.querySelector('.js-insert-nav'),
+        $sections = document.querySelectorAll('.content > section'),
+        subCounter = 0;
+
+    buildMarkup($sections);
+
+    function getFirstHeading($parent) {
+        return $parent.querySelector('h1, h2, h3, h4, h5, h6');
+    }
+
+    function buildMarkup($sections, isChildren) {
+        var $currentSection,
+            $currentHeading,
+            $childrenSections,
+            currentId = '',
+            iterator = 0,
+            sectionsLength = $sections.length,
+            navMarkup = isChildren ? '<ul>' : '<nav class="block-nav"><ul>';
+
+        subCounter += 1;
+
+        for (iterator; iterator < sectionsLength; iterator += 1) {
+            $currentSection = $sections[iterator];
+            $currentHeading = getFirstHeading($currentSection);
+            $childrenSections = $currentSection.querySelectorAll('section');
+            currentId = $currentSection.getAttribute('id');
+
+            if (!currentId) {
+                currentId = 'generated-nav-id-' + subCounter + '-' + iterator;
+                $currentSection.id = currentId;
+            }
+
+            
+
+            if ($childrenSections.length) {
+                navMarkup += '<li class="dropdown"><a href="#' + currentId + '">' + $currentHeading.innerHTML + '</a>';
+                navMarkup += buildMarkup(document.querySelectorAll('#' + currentId + ' > section'), true);
+            } else {
+                navMarkup += '<li><a href="#' + currentId + '">' + $currentHeading.innerHTML + '</a>';
+            }
+
+            navMarkup += '</li>';
+        }
+
+        navMarkup += isChildren ? '</ul>' : '</ul></nav>';
+
+        $insertNav.innerHTML = navMarkup;
+
+        return navMarkup;
+    }
+
 }());
